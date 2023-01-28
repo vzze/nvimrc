b00ecc1ba11fccc42fb08217bb2e3b6ba94084ad
vzze <lolo32778@gmail.com>
2023-01-28 19:44:07 +0200

    mapping changes

--- lua/v/mappings/global.lua
+++ lua/v/mappings/global.lua
@@ -4,50 +4,49 @@ end
 
 V.mappings.global = {
     normal = {
-        ["t"]           = { name = "+tabs"                                                           },
+        ["t"]           = { name = "Tabs"                                                            },
         ["th"]          = { "<cmd>tabprevious<CR>"                          , "Previous Tab"         },
         ["tl"]          = { "<cmd>tabnext<CR>"                              , "Next Tab"             },
         ["tn"]          = { "<cmd>tabnew<CR>"                               , "New Tab"              },
         ["td"]          = { "<cmd>tabclose<CR>"                             , "Close Tab"            },
-        ["te"]          = { "<cmd>lua require(\"FTerm\").toggle()<CR>"      , "Terminal"             },
-        ["tf"]          = { "<cmd>NeoTreeFocusToggle<CR>"                   , "File Editor"          },
+        ["te"]          = { "<cmd>lua require(\"FTerm\").toggle()<CR>"      , "Toggle Terminal"      },
+        ["tf"]          = { "<cmd>NeoTreeFocusToggle<CR>"                   , "Toggle File Editor"   },
 
-        ["s"]           = { name = "+splits"                                                         },
-        ["sq"]          = { "<cmd>wincmd q<CR>"                             , "Close"                },
-        ["sv"]          = { "<cmd>wincmd v<CR>"                             , "Vertical"             },
-        ["ss"]          = { "<cmd>wincmd s<CR>"                             , "Horizontal"           },
-
-        ["so"]          = { "<cmd>wincmd |<CR>"                             , "Max Out Width"        },
-
-        ["sl"]          = { "<cmd>wincmd 16><CR>"                           , "Increase W16"         },
-        ["sh"]          = { "<cmd>wincmd 16<<CR>"                           , "Decrease W16"         },
-        ["sL"]          = { "<cmd>wincmd 8><CR>"                            , "Increase W8"          },
-        ["sH"]          = { "<cmd>wincmd 8<<CR>"                            , "Decrease W8"          },
-
-        ["si"]          = { "<cmd>wincmd _<CR>"                             , "Max Out Height"       },
-
-        ["sk"]          = { "<cmd>wincmd 16+<CR>"                           , "Increase H16"         },
-        ["sj"]          = { "<cmd>wincmd 16-<CR>"                           , "Decrease H16"         },
-        ["sK"]          = { "<cmd>wincmd 8+<CR>"                            , "Increase H8"          },
-        ["sJ"]          = { "<cmd>wincmd 8-<CR>"                            , "Decrease H8"          },
-
-        ["s="]          = { "<cmd>wincmd =<CR>"                             , "Equalize"             },
-        ["sw"]          = { "<cmd>wincmd w<CR>"                             , "Cycle"                },
+        ["s"]           = { name = "Splits"                                                          },
+        ["sq"]          = { "<cmd>wincmd q<CR>"                             , "Close Split"          },
+        ["sv"]          = { "<cmd>wincmd v<CR>"                             , "Vertical Split"       },
+        ["ss"]          = { "<cmd>wincmd s<CR>"                             , "Horizontal Split"     },
+        ["sw"]          = { "<cmd>wincmd w<CR>"                             , "Cycle Splits"         },
         ["sr"]          = { "<cmd>CocCommand clangd.switchSourceHeader<CR>" , "Switch HH & CC"       },
 
-        ["<leader>h"]   = { "<cmd>TSToggle highlight<CR>"                   , "Toggle Highlight"     },
+        ["su"]          = { "<cmd>wincmd _<CR>"                             , "Max Out Height"       },
+        ["si"]          = { "<cmd>wincmd |<CR>"                             , "Max Out Width"        },
+        ["sp"]          = { "<cmd>wincmd =<CR>"                             , "Equalize Splits"      },
+
+        ["sl"]          = { "<cmd>wincmd l<CR>"                             , "Move to Right Split"  },
+        ["sk"]          = { "<cmd>wincmd k<CR>"                             , "Move to Top Split"    },
+        ["sj"]          = { "<cmd>wincmd j<CR>"                             , "Move to Bottom Split" },
+        ["sh"]          = { "<cmd>wincmd h<CR>"                             , "Move to Left Split"   },
+
+        ["sL"]          = { "<cmd>wincmd 6><CR>"                            , "Increase Width"       },
+        ["sH"]          = { "<cmd>wincmd 6<<CR>"                            , "Decrease Width"       },
+        ["sK"]          = { "<cmd>wincmd 4+<CR>"                            , "Increase Height"      },
+        ["sJ"]          = { "<cmd>wincmd 4-<CR>"                            , "Decrease Height"      },
+
+        ["<leader>"]    = { name = "V1602"                                                           },
+        ["<leader>h"]   = { "<cmd>TSToggle highlight<CR>"                   , "Toggle TS Highlight"  },
         ["<leader>s"]   = { "<Plug>(leap-forward-to)"                       , "Leap Forward"         },
         ["<leader>S"]   = { "<Plug>(leap-backward-to)"                      , "Leap Backward"        },
-        ["<leader>z"]   = { "<cmd>ZenMode<CR>"                              , "Zen Mode"             },
-        ["<leader>l"]   = { "<cmd>Lazy<CR>"                                 , "Plugin Manager"       },
+        ["<leader>z"]   = { "<cmd>ZenMode<CR>"                              , "Toggle Zen Mode"      },
+        ["<leader>l"]   = { "<cmd>Lazy<CR>"                                 , "Open Plugin Manager"  },
 
-        ["<leader>u"]   = { name = "+update"                                                         },
+        ["<leader>u"]   = { name = "Update"                                                          },
         ["<leader>up"]  = { "<cmd>Lazy sync<CR>"                            , "Plugins"              },
         ["<leader>ul"]  = { "<cmd>CocUpdate<CR>"                            , "Language Servers"     },
         ["<leader>ut"]  = { "<cmd>TSUpdate all<CR>"                         , "Treesitter"           },
 
-        ["<leader>t"]   = { name = "+telescope"                                                      },
-        ["<leader>tt"]  = { "<cmd>Telescope builtin<CR>"                    , "Telescope"            },
+        ["<leader>t"]   = { name = "Telescope"                                                       },
+        ["<leader>tt"]  = { "<cmd>Telescope builtin<CR>"                    , "Open Telescope"       },
         ["<leader>tf"]  = { "<cmd>Telescope find_files<CR>"                 , "Find Files"           },
         ["<leader>tg"]  = { "<cmd>Telescope live_grep<CR>"                  , "Grep Time"            },
         ["<leader>tb"]  = { "<cmd>Telescope buffers<CR>"                    , "Find Buffers"         },
@@ -57,54 +56,46 @@ V.mappings.global = {
         ["<leader>tl"]  = { "<cmd>Telescope git_branches<CR>"               , "Git Branches"         },
         ["<leader>ta"]  = { "<cmd>Telescope git_status<CR>"                 , "Git Status"           },
 
-        ["<leader>c"]   = { name = "+coc"                                                            },
+        ["<leader>c"]   = { name = "CoC"                                                             },
         ["<leader>co"]  = { "<cmd>call v:lua._Internal.ShowDocs()<CR>"      , "Shows Docs"           },
         ["<leader>cn"]  = { "<Plug>(coc-diagnostic-prev)"                   , "Diagnostic Prev"      },
         ["<leader>cm"]  = { "<Plug>(coc-diagnostic-next)"                   , "Diagnostic Next"      },
         ["<leader>cd"]  = { "<Plug>(coc-definition)"                        , "Code Definition"      },
         ["<leader>cw"]  = { "<cmd>CocDiagnostic<CR>"                        , "Diagnostic Window"    },
-        ["<leader>cy"]  = { "<Plug>(coc-type-definition)"                   , "Code Type Def"        },
-        ["<leader>ci"]  = { "<Plug>(coc-implementation)"                    , "Code Impl"            },
-        ["<leader>cr"]  = { "<Plug>(coc-references)"                        , "Code Ref"             },
+        ["<leader>cy"]  = { "<Plug>(coc-type-definition)"                   , "Code Type Definition" },
+        ["<leader>ci"]  = { "<Plug>(coc-implementation)"                    , "Code Implementation"  },
+        ["<leader>cr"]  = { "<Plug>(coc-references)"                        , "Code References"      },
         ["<leader>ca"]  = { "<Plug>(coc-codeaction)"                        , "Code Action"          },
         ["<leader>cf"]  = { "<Plug>(coc-fix-current)"                       , "Auto Fix"             },
 
-        ["<leader>cl"]  = { name = "+list"                                                           },
+        ["<leader>cl"]  = { name = "List"                                                            },
         ["<leader>cld"] = { "<cmd>CocList diagnostics<CR>"                  , "List Diagnostics"     },
         ["<leader>clo"] = { "<cmd>CocList outline<CR>"                      , "Current Doc Symbols"  },
         ["<leader>cls"] = { "<cmd>CocList -I symbols<CR>"                   , "Workspace Symbols"    },
 
         ["gc"]          = { "<cmd>CommentToggle<CR>"                        , "Comment Line"         },
 
-        ["<C-f>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"' , "Scroll Down" , expr = true },
-        ["<C-b>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"' , "Scroll Up"   , expr = true },
+        ["<C-f>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', expr = true    },
+        ["<C-b>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', expr = true    }
     },
     visual = {
-        ["gc"]          = { ":CommentToggle<CR>"                          , "Comment Block"        },
+        ["gc"]          = { ":CommentToggle<CR>"                            , "Comment Block"        },
 
-        ["<leader>s"]   = { "<Plug>(leap-forward-till)"                   , "Leap Forward"         },
-        ["<leader>S"]   = { "<Plug>(leap-backward-till)"                  , "Leap Backward"        },
+        ["<leader>s"]   = { "<Plug>(leap-forward-till)"                     , "Leap Forward"         },
+        ["<leader>S"]   = { "<Plug>(leap-backward-till)"                    , "Leap Backward"        },
 
-        ["<C-f>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"' , "Scroll Down" , expr = true },
-        ["<C-b>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"' , "Scroll Up"   , expr = true }
+        ["<C-f>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', "ScrollD", expr = true },
+        ["<C-b>"]       = { 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', "ScrollU", expr = true }
     },
     insert = {
-        ["<CR>"]        = { "v:lua._Internal.CompletionConfirm()"         , "Report Enter" , expr = true },
+        ["<CR>"]        = { "v:lua._Internal.CompletionConfirm()", "Confirm", expr = true            },
 
-        ["<TAB>"]       = {
-            'coc#pum#visible() ? coc#pum#next(1) : v:lua._Internal.CheckBackSpace() ? "<TAB>" : coc#refresh()',
-            "Report Tab", expr = true
-        },
-        ["<S-TAB>"]     = { [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], "Report S-Tab", expr = true },
-        ["<C-f>"]       = { 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"',
-            "Scroll Down"  , expr = true
-        },
-        ["<C-b>"]       = {
-            'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"',
-            "Scroll Up", expr = true
-        },
+        ["<TAB>"]       = { 'coc#pum#visible() ? coc#pum#next(1) : v:lua._Internal.CheckBackSpace() ? "<TAB>" : coc#refresh()', "TAB"    , expr = true },
+        ["<S-TAB>"]     = { [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]]                                                , "S-TAB"  , expr = true },
+        ["<C-f>"]       = { 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"'                            , "ScrollD", expr = true },
+        ["<C-b>"]       = { 'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"'                             , "ScrollU", expr = true }
     },
     terminal = {
-        ["<Esc>"]       = { wrap("<C-\\><C-n>"), "Term Normal Mode" }
+        ["<Esc>"]       = { wrap("<C-\\><C-n>"), "Terminal Escape" }
     }
 }
